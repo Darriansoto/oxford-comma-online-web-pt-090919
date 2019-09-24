85f2e897b238708f051b2214edf63c00303696fc
Darrian Soto <ddog350@gmail.com>
2019-09-24 12:26:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   counter = 1
   new_array = []
   array = ["kiwi","durian","starfruit","mangos","dragon fruits"]
-  array.each do |fruit|
+  array.collect.each_with_index do |fruit|
     
     counter += 1
   end
