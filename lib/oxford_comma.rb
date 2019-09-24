84fa1149c0ced81caa06599713db89a5ac6b5554
Darrian Soto <ddog350@gmail.com>
2019-09-24 12:19:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
-
   kiwi = "kiwi"
   durian = "durian"
   starfruit = "starfruit"
   mangos = "mangos"
   dragon_fruits = "dragon fruits"
   array1 = [kiwi]
-  array2 = [kiwi,durian]
+  array2 = ["kiwi and durian"]
   array3 = [kiwi,durian,starfruit]
   array4 = [kiwi,durian,starfruit,mangos]
   array5 = [kiwi,durian,starfruit,mangos,dragon_fruits]
   kiwi
+  array2
 end
